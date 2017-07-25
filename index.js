54490f5f5caf0a7c0ef6c8c1712d4022f2e89870
Tristan Schmitt <trschmitt50@live.com>
2017-07-25 18:44:44 -0400

    moved files around to proper spots and begun editing and formatting index and mustache files

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// const fs = require('fs');
-// const path = require('path');
-// const expressMustache = require('express-mustache');
-//
-// app.engine("mustache", mustacheExpress());
-// app.set("view engine", "mustache")
-// app.set("views", path.join(__dirname, "views") )
-
-const express = require('express');
+const path = require("path");
+const mustacheExpress = require("mustache-express");
+const express = require("express");
 const app = express();
-const data = require('data.js');
+const DATA = require("./views/data");
 
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
 
-app.listen(3010)
+app.get("/", (req, res) => {
+	res.render("index.mustache", DATA);
+});
+
+app.listen(3010, function() {
+	console.log("Successfully started App.");
+});

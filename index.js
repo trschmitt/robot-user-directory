86c099c736314122310eee245d7f2df512bada47
Tristan Schmitt <trschmitt50@live.com>
2017-07-25 16:15:13 -0400

    Changed location of html files and started JS files

---
+++ index.js
@@ -0,0 +1,14 @@
+// const fs = require('fs');
+// const path = require('path');
+// const expressMustache = require('express-mustache');
+//
+// app.engine("mustache", mustacheExpress());
+// app.set("view engine", "mustache")
+// app.set("views", path.join(__dirname, "views") )
+
+const express = require('express');
+const app = express();
+const data = require('data.js');
+
+
+app.listen(3010)

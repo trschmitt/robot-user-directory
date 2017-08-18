5b2e8d3fdc6df0469c7c4143ff7f9991c25ddd2e
Tristan Schmitt <trschmitt50@live.com>
2017-08-18 12:06:03 -0400

    Finished Project

--- index.js
+++ index.js
@@ -5,24 +5,36 @@ const app = express();
 const DATA = require("./views/data");
 const fs = require("fs");
 
+app.set("port", process.env.PORT || 3010);
+
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-	res.render("users.mustache", DATA);
-});
-
-app.get("/:username", (req, res) => {
-	let foundUser = DATA.users.find(
-		user => user.username === req.params.username
-	);
-
-	res.render("personal", { users: foundUser });
-});
-
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.listen(3010, function() {
-	console.log("Successfully started App.");
+
+app.get("/", (req, res) => {
+  res.render("users", DATA);
+});
+
+app.get("/:id", (req, res) => {
+  let foundUser = DATA.users.find(user => {
+    return user.id == req.params.id;
+  });
+
+  res.render("personal", foundUser);
+});
+
+
+app.listen(app.get("port"), err => {
+  if (err) {
+    throw err;
+    exit(1);
+  }
+  console.log(
+    `Node running in ${app.get("env")} mode @ http://localhost:${app.get(
+      "port"
+    )}`
+  );
 });
